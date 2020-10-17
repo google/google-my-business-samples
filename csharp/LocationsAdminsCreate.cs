97ae079f3c781d27f9786c73201927661bdd3618
Aswin Thomas <aswinthomas@gmail.com>
2020-10-17 11:46:24 +0800

    add account admin example csharp

---
+++ csharp/LocationsAdminsCreate.cs
@@ -0,0 +1,68 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.MyBusiness.v4;
+using Google.Apis.Services;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Net.Http;
+
+
+public class LocationsAdminsCreate
+{
+    public class WebCredentials
+    {
+        public class Web
+        {
+            public string client_id { set; get; }
+            public string client_secret { set; get; }
+        }
+        public Web web { get; set; }
+
+    }
+
+    static void Main(string[] args)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "\\client_secrets.json");
+        Console.WriteLine("Reading credentials from " + path);
+        string contents = File.ReadAllText(path);
+        var deserializedCredentials = JsonConvert.DeserializeObject<WebCredentials>(contents);
+
+        var ClientId = deserializedCredentials.web.client_id;
+        var ClientSecret = deserializedCredentials.web.client_secret;
+        Console.WriteLine("Obtained client id " + ClientId + " and secret " + ClientSecret);
+        string[] scopes = new string[] { "https://www.googleapis.com/auth/plus.business.manage" };
+
+        var userCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            new ClientSecrets
+            {
+                ClientId = ClientId,
+                ClientSecret = ClientSecret,
+            },
+             scopes,
+             "user",
+             CancellationToken.None).Result;
+
+        var service = new MyBusinessService(new BaseClientService.Initializer() { HttpClientInitializer = userCredential });
+        service.HttpClient.DefaultRequestHeaders.Add("X-GOOG-API-FORMAT-VERSION", "2");
+        var accountsListResponse = service.Accounts.List().Execute();
+        Console.WriteLine("Choosing account: " + JsonConvert.SerializeObject(accountsListResponse.Accounts[9]));
+
+        var account = accountsListResponse.Accounts[9];
+        var locationListResponse = service.Accounts.Locations.List(account.Name).Execute();
+        Console.WriteLine("\n\nChoosing location: " + JsonConvert.SerializeObject(locationListResponse.Locations[0]));
+
+        var location = locationListResponse.Locations[0];
+        var admin = new Google.Apis.MyBusiness.v4.Data.Admin();
+        admin.Name = location.Name;
+        admin.AdminName = "xyzhh@gml.com";
+        admin.Role = "MANAGER";
+        Console.WriteLine("\n\nCreated Admin Object " + JsonConvert.SerializeObject(admin));
+        Console.WriteLine("\n\nInviting manager for location: " + location.Name);
+        var locationsAdminsCreateRequest = service.Accounts.Locations.Admins.Create(admin, location.Name);
+        Console.WriteLine("\n\nRequest: " + JsonConvert.SerializeObject(locationsAdminsCreateRequest));
+        var locationsAdminsCreateResponse = locationsAdminsCreateRequest.Execute();
+        Console.WriteLine("\n\nResponse: " + JsonConvert.SerializeObject(locationsAdminsCreateResponse));
+    }
+
+}
--- csharp/README.md
+++ csharp/README.md
@@ -1,4 +1,6 @@
-## Pre-requisites
+
+## Generating Client Library
+### Pre-requisites
 
 You will need a windows machine to complete these steps
 
@@ -7,17 +9,19 @@ Install the following:
 - Visual Studio
 - [git](https://git-scm.com/download/win)
 - [Python 2.7](https://docs.python-guide.org/starting/install/win/) and setup environment in win powershell 
-- Setup pip `curl https://bootstrap.pypa.io/ez_setup.py | python` and `curl https://bootstrap.pypa.io/get-pip.py | python`
+- Setup pip
+```
+curl https://bootstrap.pypa.io/ez_setup.py | python
+curl https://bootstrap.pypa.io/get-pip.py | python`
+```
 - Install dependencies with pip
 ```
 pip install --index-url https://test.pypi.org/simple/ 'httplib2==0.17.5'
 pip install Django==1.8.1
-pip install google-api-python-client
-pip install google-apputils
-pip install python-gflags
+pip install google-api-python-client google-apputils python-gflags
 ```
 
-## Generate VS solution
+### Generate VS solution
 
 Execute the following steps in a new powershell to generate the VS project
 
@@ -26,5 +30,12 @@ Execute the following steps in a new powershell to generate the VS project
 - `./BuildGenerated.sh --skipdownload`
 
 
+## Running samples
+
+- Open the generated CS project
+- Create a file named client_secrets.json, with the credentials downloaded as JSON from your Google Cloud Project API Console. This should be an application OAuth.
+- Set project to run as net45. This could involve removing other version references from the .csproj file
+- You may need to set SignAssembly to false if you get an snk error
+- Open the project in Visual Studio and install missing nuget packages
 
 
