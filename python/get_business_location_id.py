5492bf7f5d376efd923764c9976cda906ff03979
Francisco Blasco Garma <fblascogarma@gmail.com>
2022-02-08 10:39:38 -0800

    edits to set user credentials outside of the file.

--- python/get_business_location_id.py
+++ python/get_business_location_id.py
@@ -16,62 +16,39 @@ This is an example to get the business_location_id of a user's Business Profile.
 This is used to create Smart Campaigns, an advertising type in Google Ads.
 
 Prerequisites:
-1. Install Google API Client Discovery
-python -m pip install google-api-python-client
-2. Get your Google Cloud Platform (GCP) project approved.
+1. Get your Google Cloud Platform (GCP) project approved.
 Fill out this form:
 https://docs.google.com/forms/d/1XTQc-QEjsE7YrgstyJxbFDnwmhUhBFFvpNJBw3VzuuE/viewform  
 Find out more information here:
 https://developers.google.com/my-business/content/prereqs
-3. In that approved project, you need to enable two APIs:
+2. In that approved project, you need to enable two APIs:
 My Business Business Information API. There isn’t a typo; ‘business’ is repeated twice.
 My Business Account Management API
-4. Get a refresh token from a user's Google account with the following scope:
-'https://www.googleapis.com/auth/business.manage'
 '''
-import os
+import sys
 
-from googleapiclient.discovery import build
-import google.oauth2.credentials
+from googleapiclient import sample_tools
 
-def business_profile(refresh_token):
+def main(argv):
 
     try:
         '''
         Step 1 - Configurations
         '''
-        # env variables
-        GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-        GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-
-        # documentation that explains the fields of the credentials
-        # https://google-auth.readthedocs.io/en/stable/reference/google.oauth2.credentials.html
-        credentials = {
-            'refresh_token': refresh_token,
-            'client_id': GOOGLE_CLIENT_ID,
-            'client_secret': GOOGLE_CLIENT_SECRET,
-            'token': None,
-            'token_uri': "https://oauth2.googleapis.com/token"
-        }
-
-        # build the credentials object
-        google_credentials = google.oauth2.credentials.Credentials(**credentials)
+        # Use the discovery doc to build two services that we can use to make
+        # MyBusiness API (aka Business Profile API) calls, 
+        # and authenticate the user so we can access their account
+        MyBusinessAccount, flags = sample_tools.init(argv, "mybusinessaccountmanagement", "v1", __doc__, __file__, scope="https://www.googleapis.com/auth/business.manage")
+        MyBusinessInformation, flags = sample_tools.init(argv, "mybusinessbusinessinformation", "v1", __doc__, __file__, scope="https://www.googleapis.com/auth/business.manage")
 
         '''
         Step 2 - Get the account information using the Account Managment API
         https://developers.google.com/my-business/reference/accountmanagement/rest
         '''
-        # start the service
-        service = build(
-            'mybusinessaccountmanagement',      # serviceName
-            'v1',                               # version
-            credentials=google_credentials      # user's credentials
-            )
-        
-        # build the request to accounts using list method
-        request = service.accounts().list()
+        # Build the request to accounts using list method
+        request = MyBusinessAccount.accounts().list()
 
-        # execute the request and print the result
+        # Execute the request and print the result
         result = request.execute()
         print("result:")
         print(result)
@@ -81,32 +58,28 @@ def business_profile(refresh_token):
 
         '''
         Step 3 - Get the business information using the Business Information API
+        This example gets the business_location_id, but you can also get more information.
+        Below is the link to the Business Information API
         https://developers.google.com/my-business/reference/businessinformation/rest/v1/accounts.locations/list
+        Below is the link to see the fields you can get using this API
+        https://developers.google.com/my-business/reference/businessinformation/rest/v1/accounts.locations#Location
         '''
-        # start the service
-        service = build(
-            'mybusinessbusinessinformation',    # serviceName
-            'v1',                               # version
-            credentials=google_credentials      # user's credentials
-            )
+        # List the fields you want to get, separated by a comma
+        # For example: 'name,title,websiteUri,languageCode,phoneNumbers'
+        fields_we_want = 'name'
 
-        # set the fields you want to get of the Business Information profile
-        # follow the link below to see all the fields you can get
-        # https://developers.google.com/my-business/reference/businessinformation/rest/v1/accounts.locations#Location
-        fields_we_want = 'name,title,websiteUri,languageCode,phoneNumbers'
-
-        # build the request to accounts.locations using list method
-        request = service.accounts().locations().list(
+        # Build the request to accounts.locations using list method
+        request = MyBusinessInformation.accounts().locations().list(
             parent=account,
             readMask=fields_we_want
             )
 
-        # execute the request and print the result
+        # Execute the request and print the result
         result = request.execute()
         print("result:")
         print(result)
 
-        # get the business_location_id
+        # Get the business_location_id
         business_location_id = result['locations'][0]['name'].split('/')[1]
         print("business_location_id:")
         print(business_location_id)
@@ -115,4 +88,7 @@ def business_profile(refresh_token):
 
     except:
         print('There was an error trying to get Business Information.')
+
+if __name__ == "__main__":
+  main(sys.argv)
         
